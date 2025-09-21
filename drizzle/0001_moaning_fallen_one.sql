c3cb7c6ffb9aef67386bf38fdda7d89d0ee0e702
jmessk <jmerusty@gmail.com>
2025-09-21 16:37:00 +0000

    Add test migration

--- drizzle/0001_moaning_fallen_one.sql
+++ drizzle/0001_moaning_fallen_one.sql
@@ -1,10 +1,20 @@
 -- Custom SQL migration file, put your code below! --
-INSERT INTO "posts" ("id", "title", "description", "content", "created_at") VALUES (
+INSERT INTO "posts" ("id", "title", "description", "thumbnail_url", "content", "created_at") VALUES (
   '0',
-  'test-post',
+  'Test Post',
   'This is a test post.',
-  '# Test Post
+  'https://rustacean.net/assets/rustacean-orig-noshadow.png',
+  '## Test Post
   
-  This is a test post.',
+  This is a test post.
+  ',
   '2000-01-01T00:00:00Z'
 );
+
+INSERT INTO "tags" ("id", "name", "icon_url") VALUES (
+  'rust',
+  'Rust',
+  'https://www.rust-lang.org/static/images/rust-logo-blk.svg'
+);
+
+INSERT INTO "post_tags" ("post_id", "tag_id") VALUES ('0', 'rust');
