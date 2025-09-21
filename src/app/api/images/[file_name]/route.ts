0646f779fa29ad65dc3353e9c882526194e39f19
jmessk <jmerusty@gmail.com>
2025-09-21 16:53:45 +0000

    Fix api

--- src/app/api/images/[file_name]/route.ts
+++ src/app/api/images/[file_name]/route.ts
@@ -1,16 +1,15 @@
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { NextRequest, NextResponse } from "next/server";
-import path from "path";
 
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ file_name: string }> }) {
   const { env } = getCloudflareContext();
 
-  const R2_IMAGES_URL = env.R2_IMAGES_URL;
-
   const { file_name } = await params;
-  const image_id = path.parse(file_name).name;
   const options = { cf: { images: {} } };
 
-  return await fetch(`${R2_IMAGES_URL}/${image_id}`, options);
+  // https://<R2_IMAGES_URL>/<file_name>
+  const url = new URL(file_name, env.R2_IMAGES_URL);
+
+  return await fetch(url.toString(), options);
 }
